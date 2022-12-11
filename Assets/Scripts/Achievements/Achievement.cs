e77a8d2296ff6ab4517772b5c608d8c4b72d9707
Sarmatt <102482656+Sarmatt@users.noreply.github.com>
2022-12-11 23:44:49 +0200

    add comment 2

--- Assets/Scripts/Achievements/Achievement.cs
+++ Assets/Scripts/Achievements/Achievement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Achievement/Create new")]
 public class Achievement : ScriptableObject
 {
-    public string Title;//Hello
+    public string Title;//Hello Hello 2
     [TextArea]
     public string Text;
     public Sprite Image;
