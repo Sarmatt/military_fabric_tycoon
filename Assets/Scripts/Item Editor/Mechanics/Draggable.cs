a25f3c64a9c333e0962e990d056b2c5c27a2ec88
Vitalik Kravets <vitalikkravez1@gmail.com>
2022-12-15 22:45:51 +0200

    edit Draggable.cs script

--- Assets/Scripts/Item Editor/Mechanics/Draggable.cs
+++ Assets/Scripts/Item Editor/Mechanics/Draggable.cs
@@ -4,30 +4,30 @@
 public class Draggable : MonoBehaviour
 {
     private Vector3 _startPosition;
-    private Transform _transform;
     private bool _isDraggable;
     private void Start()
     {
-        _startPosition = _transform.position;
-        _isDraggable = false;
+        _startPosition = transform.position;
+        _isDraggable = true;
     }
     private void OnMouseDown()
     {
-        _isDraggable = true;
+        Start();
     }
     private void OnMouseDrag()
     {
-        Vector3 position = new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.WorldToScreenPoint(_transform.position).z);
+        if (!_isDraggable) return;
+        Vector3 position = new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.WorldToScreenPoint(transform.position).z);
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-        _transform.position = new Vector3(worldPosition.x,worldPosition.y,worldPosition.z);
+        transform.position = new Vector3(worldPosition.x,worldPosition.y,worldPosition.z);
     }
     private void OnMouseUp()
     {
-        if (_isDraggable) return;
-        _transform.position = _startPosition;
+        EndMoving();
     }
     public void EndMoving()
     {
+        transform.position = _startPosition;
         _isDraggable = false;
     }
 }
